eafe002f1822b5dce24fe38806c92317c229d398
Robinson Greig <robinsongreig@gmail.com>
2017-05-10 08:39:30 -0400

    add color and font variables

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  root 'records#index'
   resources :records
-  resources :code_amendments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
